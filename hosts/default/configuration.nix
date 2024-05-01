3a526cc83c7901a1bebf1e2871b13bb7f607261e
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-01 18:29:11 +0200

    79 current  2024-05-01 18:29:06  24.05.20240427.58a1abd       6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -39,12 +39,14 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "deekahy";
+  programs.hyprland.enable = true; 
 
-  services.xserver.desktopManager.plasma5.enable = true;
+  # # Enable the KDE Plasma Desktop Environment.
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.autoLogin.enable = true;
+  # services.displayManager.autoLogin.user = "deekahy";
+  #
+  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
