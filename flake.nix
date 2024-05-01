9d8de4851c7a3387f32d50b2c867a46ca3551589
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-01 18:23:04 +0200

    78 current  2024-05-01 16:43:30  24.05.20240427.58a1abd       6.6.28                          *

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +15,6 @@
       modules = [
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
-        
       ];
     };
   };
