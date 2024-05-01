f05a6dc92b563e2129d052013d37695655e9e5a7
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-01 15:05:02 +0200

    69 current  2024-05-01 15:04:57  24.05.20240427.58a1abd       6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -1,37 +1,72 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  nix.settings.experimental-features = "nix-command flakes";
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./deekahy.nix
+      inputs.home-manager.nixosModules.default
+    ];
 
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set root password (optional, if you need to reset it)
-  users.users.root.password = "newrootpassword";
+  # Set your time zone.
+  time.timeZone = "Europe/Copenhagen";
 
-  # Ensure the user 'deekahy' is correctly set up
-  users.users.deekahy = {
-    isNormalUser = true;
-    home = "/home/deekahy";
-    password = "sunsil";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_DK.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
   };
 
-  # Enable a graphical environment: Xfce
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-  ];
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "deekahy";
 
-  # Basic system settings
-  system.stateVersion = "unstable";
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.variant = "";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+
+  system.stateVersion = "unstable"; # Did you read the comment?
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 }
+
