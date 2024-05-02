1b15b7a3f6c67571388e55f42bd51116ff51ff9e
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-02 12:46:34 +0200

    101 current  2024-05-02 12:46:30  24.05.20240427.58a1abd       6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -101,7 +101,10 @@ programs.hyprland = {
   enable = true;
   xwayland.enable = true;
 };
-
+environment.sessionVariables = {
+  # WLR_NO_HARDWARE_CURSORS = "1";
+  NIXOS_OZONE_WL = "1";
+};
 
   hardware.opengl = {
     enable = true;
