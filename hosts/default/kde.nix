bea580accf6c0341d2d078da5172392408880bc4
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-06 21:13:36 +0200

    init

---
+++ hosts/default/kde.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+{
+
+# # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "deekahy";
+  
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+  }
+  
