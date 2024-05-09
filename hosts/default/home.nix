dccac14e54a1c74eb3bbd284b26b87914e9f7cab
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-09 13:36:08 +0200

    136 current  2024-05-09 13:36:07  24.05.20240505.25865a4  6.6.30                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -71,6 +71,8 @@
 
 # fun
     cmatrix
+    nushell
+    starship
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
