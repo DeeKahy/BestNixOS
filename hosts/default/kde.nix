0e59925cc7aa8917d82c731b7e2c3777a121ed9e
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-10 14:15:34 +0200

    148 current  2024-05-10 14:15:32  24.05.20240509.f1010e0  6.6.30                          *

--- hosts/default/kde.nix
+++ hosts/default/kde.nix
@@ -4,10 +4,10 @@
 {
 
 # # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "deekahy";
+  # services.displayManager.autoLogin.enable = true;
+  # services.displayManager.autoLogin.user = "deekahy";
   
   services.xserver.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
+  # services.xserver.displayManager.defaultSession = "plasma";
   }
   
