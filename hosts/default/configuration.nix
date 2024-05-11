4f37896a10fdbb408a42b93ee32f276a64f40fa9
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-11 14:41:16 +0200

    175 current  2024-05-11 14:41:15  24.05.20240509.f1010e0  6.6.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -7,7 +7,7 @@
     [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.default
-    # ./kde.nix
+    ./kde.nix
     ];
 
 # my user
@@ -59,11 +59,6 @@
 # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
-
-  services.xserver.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
-
 
 # Configure keymap in X11
   services.xserver.xkb.layout = "us";
@@ -111,11 +106,16 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
 
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
   environment.systemPackages = with pkgs; [
     nh
     nix-output-monitor
+    nvd
     protonup
-    mangohud
+      mangohud
   ];
 
   programs.nh = {
@@ -128,6 +128,8 @@
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/deekahy/.steam/root/compatibilitytools.d";
     JAVA_HOME = "/nix/store/jnvh76s6vrmdd1rnzjll53j9apkrwxnc-openjdk-21+35";
+    NIXOS_OZONE_WL = "1";
+# SDL_VIDEODRIVER = "wayland";
   };
 
   system.stateVersion = "unstable"; # Did you read the comment?
