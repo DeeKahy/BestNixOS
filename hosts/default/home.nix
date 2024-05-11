7f38c3d60bdf42efc4fce00cbe9b72e182ea9238
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-11 20:38:15 +0200

    something

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -6,6 +6,10 @@
   home.username = "deekahy";
   home.homeDirectory = "/home/deekahy";
 
+  imports =
+    [ # Include the results of the hardware scan.
+    ./firefox.nix
+    ];
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
