b8325e6893705655eb1e95189510f0a1d6b08608
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-13 01:03:55 +0200

    203 current  2024-05-13 00:55:39  24.05.20240511.062ca2a  6.6.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -96,7 +96,7 @@
     driSupport = true;
     driSupport32Bit = true;
   };
- hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+ hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
 
