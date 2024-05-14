72717fa1497cace0af6b899c2e062b62e09f6c97
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-14 21:52:22 +0200

    216 current  2024-05-14 21:34:21  24.05.20240512.2057814  6.6.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -1,26 +1,26 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+  # Enable experimental Nix features
   nix.settings.experimental-features = "nix-command flakes";
-  #import shit
-  imports =
-    [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.default
-    # ./nvidia.nix
-   # ./kde.nix
-    ];
 
-# my user
+  # Import necessary configurations
+  imports = [
+    ./hardware-configuration.nix          # Include hardware configuration
+    inputs.home-manager.nixosModules.default # Home Manager module
+    # ./nvidia.nix                         # NVIDIA configuration (commented out)
+    # ./kde.nix                            # KDE configuration (commented out)
+  ];
+
+  # User configuration
   users.users.deekahy = {
     isNormalUser = true;
     description = "DeeKahy";
     extraGroups = [ "networkmanager" "wheel" "disk" ];
-    packages = with pkgs; [
-    ];
-
+    packages = with pkgs; [ ];
   };
 
+  # Home Manager configuration
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users = {
@@ -28,22 +28,21 @@
     };
   };
 
-
-# Bootloader.
+  # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  # Hostname configuration
+  networking.hostName = "nixos";
 
-# Enable networking
-    networking.networkmanager.enable = true;
+  # Enable NetworkManager for networking
+  networking.networkmanager.enable = true;
 
-# Set your time zone.
+  # Set timezone
   time.timeZone = "Europe/Copenhagen";
 
-# Select internationalisation properties.
+  # Internationalization settings
   i18n.defaultLocale = "en_DK.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
     LC_IDENTIFICATION = "da_DK.UTF-8";
@@ -56,31 +55,33 @@
     LC_TIME = "da_DK.UTF-8";
   };
 
-# Enable the X11 windowing system.
+  # X11 windowing system configuration
   services.xserver = {
-  enable = true;
-  layout = "us";
-  displayManager.sessionCommands = ''
-    # Set up monitors with appropriate refresh rates
-    xrandr --output DP-0 --mode 2560x1440 --rate 143.91
-    xrandr --output HDMI-0 --mode 1920x1080 --rate 60.00
-  '';
-};
-  services.displayManager.sddm.enable = true;
+    enable = true;
+    layout = "us";
+    displayManager.sessionCommands = ''
+      # Set up monitors with appropriate refresh rates
+      xrandr --output DP-0 --mode 2560x1440 --rate 143.91
+      xrandr --output HDMI-0 --mode 1920x1080 --rate 60.00
+    '';
+  };
 
+  # Display manager configuration
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
 
+  # Video driver configuration
   services.xserver.videoDrivers = [ "nvidia" ];
-  # services.xserver.videoDrivers = [ "nvd" ];
-  #
-# Configure keymap in X11
+  # services.xserver.videoDrivers = [ "nvd" ];  # Alternative driver (commented out)
+
+  # X11 keymap configuration
   services.xserver.xkb.layout = "us";
   services.xserver.xkb.variant = "";
 
-# Enable CUPS to print documents.
+  # Enable CUPS for printing
   services.printing.enable = true;
 
-# Enable sound with pipewire.
+  # Enable sound with PipeWire
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -91,44 +92,42 @@
     pulse.enable = true;
   };
 
+  # Enable Steam
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
   };
- programs.gamemode.enable = true;
 
-  programs.kdeconnect.enable = true; 
+  # Enable GameMode
+  programs.gamemode.enable = true;
 
+  # Enable KDE Connect
+  programs.kdeconnect.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-
+  # OpenGL hardware acceleration
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
   };
 
-
-  # programs.nix-ld.libraries = with pkgs; [];
-
-  # fonts.packages = with pkgs; [
-    # (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-  # ];
-
+  # System packages
   environment.systemPackages = with pkgs; [
     nh
-      nix-output-monitor
-      protonup
-      mangohud
-      libsForQt5.qtstyleplugin-kvantum
-    # (pkgs.nerdfonts.override { fonts = [ "jetbrains-mono" ]; })
-    # nvd
+    nix-output-monitor
+    protonup
+    mangohud
+    libsForQt5.qtstyleplugin-kvantum
     xorg.xrandr
     arandr
   ];
 
+  # NH program configuration
   programs.nh = {
     enable = true;
     clean.enable = true;
@@ -136,15 +135,17 @@
     flake = "/home/deekahy/dotfiles/nixos";
   };
 
+  # Session variables
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/deekahy/.steam/root/compatibilitytools.d";
     JAVA_HOME = "/nix/store/jnvh76s6vrmdd1rnzjll53j9apkrwxnc-openjdk-21+35";
     NIXOS_OZONE_WL = "1";
-# SDL_VIDEODRIVER = "wayland";
   };
 
+  # System state version
   system.stateVersion = "unstable"; # Did you read the comment?
-    hardware.bluetooth.enable = true; # enables support for Bluetooth
-    hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-}
 
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+}
