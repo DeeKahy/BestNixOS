d70e00e3e910c5074dc6b443255b8135902aa93d
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-14 22:15:17 +0200

    216 current  2024-05-14 21:34:21  24.05.20240512.2057814  6.6.30                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -79,6 +79,7 @@
 # fun
     cmatrix
     handbrake
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
