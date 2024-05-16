1ead31418baeba04b9646bc40990b9039d79d6ec
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-16 11:09:28 +0200

    220 current  2024-05-15 20:58:36  24.05.20240512.2057814  6.6.30                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -80,6 +80,7 @@
     cmatrix
     handbrake
     telegram-desktop
+    gimp
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
