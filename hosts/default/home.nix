646192133b9f1871e633f6565b2a81cbe2c7f80b
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-16 19:32:13 +0200

    222 current  2024-05-16 19:32:13  24.05.20240515.33d1e75  6.6.30                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -81,6 +81,7 @@
     handbrake
     telegram-desktop
     gimp
+    grapejuice
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
