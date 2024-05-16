29a30b58c2d18edf8e64687c8930b8525a587d8f
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-16 22:47:17 +0200

    229 current  2024-05-16 22:47:17  24.05.20240515.33d1e75  6.6.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -120,6 +120,7 @@
   environment.systemPackages = with pkgs; [
     nh
     
+    waydroid
     nix-output-monitor
     protonup
     mangohud
--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -81,7 +81,6 @@
     handbrake
     telegram-desktop
     gimp
-    waydroid
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
