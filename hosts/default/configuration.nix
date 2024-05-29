b3eb118d38fe18788c64ac6833ba3d01a05acfd6
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-29 17:51:53 +0200

    308 current  2024-05-29 17:51:51  24.11.20240524.bfb7a88  6.6.31                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -87,15 +87,18 @@ in
 
   # Enable sound with PipeWire
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      jack.enable = true;
+    };
   };
-
   # Enable Steam
   programs.steam = {
     enable = true;
