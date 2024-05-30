2bb1f3b8a74dfd6c94a6e0bbbe4adb2a32d00cd8
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-05-30 14:07:28 +0200

    317 current  2024-05-30 14:07:26  24.11.20240524.bfb7a88  6.6.31                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -40,7 +40,11 @@ programs.nix-ld.enable = true;
 programs.nix-ld.libraries = with pkgs; [
   # Add any missing dynamic libraries for unpackaged 
   # programs here, NOT in environment.systemPackages
-  
+   glibc
+   gcc
+   zlib
+   gcc.libc
+
 ];
 
   # Bootloader configuration
