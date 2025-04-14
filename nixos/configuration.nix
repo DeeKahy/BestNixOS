1b9835dbef942320651a8cb3999e2d1780ecd05a
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2025-04-14 19:00:45 +0200

    aaa

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,6 +115,7 @@
     prismlauncher
     distrobox
     boxbuddy
+cudaPackages.cudatoolkit
   ];
 
   # Enable Steam
--- nixos/systemconfig/displaymanager.nix
+++ nixos/systemconfig/displaymanager.nix
@@ -7,12 +7,13 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
 
-  # services.displayManager.cosmic-greeter.enable = true;
   # services.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
   services.desktopManager.plasma6.enable = true;
+  services.desktopManager.cosmic.enable = true;
 
   # services.desktopManager.cosmic.enable = true;
   # Resolve the conflict for `ssh-askpass`
